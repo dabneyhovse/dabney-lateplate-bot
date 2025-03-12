a7f1ddc7971112653bde677c6f71a1e366cf7ec4
Sammy Hajhamid <sh@pepsi.pw>
2025-03-12 08:19:52 -0700

    yep

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import TelegramBot from 'node-telegram-bot-api';
-import cron from 'node-cron';
+import TelegramBot from "node-telegram-bot-api";
+import cron from "node-cron";
 import dotenv from "dotenv";
-import { fetchMenu, fetchPrettyMenu } from "@torus/cds-api";
+import { fetchPrettyMenu } from "@torus/cds-api";
 
 dotenv.config();
 
@@ -9,32 +9,47 @@ const token = process.env.BOT_TOKEN;
 const chatId = process.env.CHAT_ID;
 const sheetsApiKey = process.env.SHEETS_API_KEY;
 
-console.log(sheetsApiKey)
-
 const bot = new TelegramBot(token, { polling: true });
 
 const formatMenu = (menu) => {
-    return Object.keys(menu).map(key => {
-        return key.charAt(0).toUpperCase() + key.slice(1) + ':\n' + menu[key].map((item) => item.dish).join('\n');
-    }).join('\n\n');
+    return Object.keys(menu)
+        .map((key) => {
+            return (
+                "__" +
+                key.charAt(0).toUpperCase() +
+                key.slice(1) +
+                "__:\n" +
+                menu[key].map((item) => item.dish).join("\n")
+            );
+        })
+        .join("\n\n");
 };
 
 const postMenu = () => {
-    const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    const daysOfWeek = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+    ];
     const today = new Date().getDay();
     const dayOfWeek = daysOfWeek[today];
 
-    fetchPrettyMenu(sheetsApiKey).then(menu => {
+    fetchPrettyMenu(sheetsApiKey).then((menu) => {
         const menuForToday = menu[dayOfWeek];
-        bot.sendMessage(chatId, formatMenu(menuForToday));
+        bot.sendMessage(chatId, formatMenu(menuForToday), {
+            parse_mode: "MarkdownV2"
+        });
     });
 };
 
-cron.schedule('0 8 * * 1-5', () => {
+cron.schedule("0 6 * * 1-5", () => {
     postMenu();
 });
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'i love lateplates');
+    bot.sendMessage(msg.chat.id, "i love lateplates");
 });
-
